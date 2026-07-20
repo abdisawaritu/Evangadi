259281d02bedafec0383ee3ff508574c986f0e43
Abdisa Waritu Ebiso <abdisawaritu4@gmail.com>
2026-07-20 00:39:49 -0700

    updated the js practice

--- Phase-2/Week-8/ProgrammingBasics/JS/script.js
+++ Phase-2/Week-8/ProgrammingBasics/JS/script.js
@@ -1,35 +1,106 @@
 // alert(" your js file is connected");
 
 // Introduction to programming basics - pratice exercise  homework
-console.log(24 > 3);
-console.log(2 > "12");
-console.log(0 == 2);
-console.log(2.0 == 2);
-console.log(2.0 == "2");
-console.log(2 < "John");
-console.log(2 > "John");
-console.log("2" < "2");
-console.log("2" > "12");
-console.log(1 == 1 || 3 == 2 || 3 == 7);
-console.log(1 == 1 && 2 == 2 && 3 == 7);
-console.log(1 == 1 || (2 == 2 && 3 == 7));
+console.log(24 > 3); //  true
+// since both are  the same data types js just compare the values
+
+console.log(2 > "12"); // false
+// the string 12 is automatically converted to the number 12 then js compare the two number  2 and 12
+
+console.log(0 == 2); // false
+// the loose equlaity just compare the values only
+console.log(2.0 == 2); //  true
+// the loose equlaity just compare the values only
+console.log(2.0 == "2"); // true
+// the loose equlaity just compare the values only
+console.log(2 < "John"); // false
+// because  the the string john is not converted to number and is NaN
+console.log(2 > "John"); // false
+// because  the the string john is not converted to number and is NaN
+console.log("2" < "2"); //false
+// because the ASCI code for the both of the string is the same and equal
+console.log("2" > "12"); // true
+// because js compare the values in inside the string character by character by convering the values into equivalent ASCII CODE
+
+console.log(1 == 1 || 3 == 2 || 3 == 7); // true
+// during the or  if js get one values  true it does not check  others is answer going to to be true
+console.log(1 == 1 && 2 == 2 && 3 == 7); // false
+// during the and   if js get one values  false it does not check  others its always false
+console.log(1 == 1 || (2 == 2 && 3 == 7)); //true  the same reason applies to this also
 console.log(
   (1 == true && 0 > true) ||
     "31" > "9" ||
     10 > 5 ||
-    !("2" == "two" || 1 == "1"),
+    !("2" == "two" || 1 == "1"), // true   the same reason applies to this also as above
 );
 
 //homework question two
+//  1. Which expression returns true?
+// A/ ‘1’ === 1
+// B/ 1 == 1
+// C/ 1 === 1
+// D/ B and C from above    this is the correct answer
+
+// 2. What is the value of x in this statement?
+// let x = (1 == true);
+// A/ 1
+// B/ true    this is the correct answer 
+// C/ false
+// D/ undefined
+
+// 3. What is the value of y from the following statements?
+// let x = 10;
+// let y = (x > 5) && (x < 15)
+// A/ 10
+// B/ 5
+// C/ 15
+// D/ true   this is the correct answer 
+
+// 4. What is the value of x from the following statements?
+// let x = 5;
+// x += 3;
+// A/ 3
+// B/ 8  this is the correct answer
+// C/ 15
+// D/ 5
+
+// 5. What is the value of y from the following statements?
+// let x = 10;
+// let y = x++;
+// A/ 10   this is the correct answer
+// B/ 11
+// C/ 12
+// D/ 13
+// 6. What is the value of y in the following statements?
+// let x = 1;
+// let y = (x !== 2);
+// A/ 1
+// B/ 2
+// C/ false
+// D/ true   this is the correct answer
+
+// 7. What is the output of (+”2”+2)?   // 4
+// 8. What is the output of (7 % 3)? // 1
+// 9. What is the output of (2+true)? // 3
+
+
+
+
+
 
 // homework question three
-let num1 = 1;
-let num2 = 2;
+let numberOne = 1;
+let numberTwo = 2;
 let sum = num1 + num2;
 console.log(sum);
+
+
 
 //homework question four
 let firstName = "abdisa";
 let lastName = "waritu";
 let fullName = firstName + " " + lastName;
 console.log(fullName);
+
+// rough paper
+console.log(Number("john "));
--- Phase-2/Week-8/javascriptPractice/JS/script.js
+++ Phase-2/Week-8/javascriptPractice/JS/script.js
@@ -1,24 +1,24 @@
-console.log("Hello world ");
-console.log("Abebe kebede");
+// console.log("Hello world ");
+// console.log("Abebe kebede");
 
 // console.log("the sum is " + " " +5 +7 );
 
-let salary = 10000;
-var demoz = 1800;
+// let salary = 10000;
+// var demoz = 1800;
 // let salary = "abdisa"
-console.log(salary);
+// console.log(salary);
 
 // declaring variabl in // variable intialization
-let y = 50;
-var d = 40;
-const p = 10;
+// let y = 50;
+// var d = 40;
+// const p = 10;
 
-console.log(y + d);
-console.log(" show we the result ");
-d = "dog";
-y = "40";
-console.log(y);
-console.log(d);
+// console.log(y + d);
+// console.log(" show we the result ");
+// d = "dog";
+// y = "40";
+// console.log(y);
+// console.log(d);
 
 // differernt types of the erro in js
 //syntac error
@@ -32,12 +32,13 @@ console.log(d);
 //primitive and non primitive data types   linear and nonlinear 
 
 
-var num1 = 40;
-let num2= 13;
-let sum= num1+num2;
-console.log(sum);
-let product = num1 * num2 ;
-console.log(product);
+// var num1 = 40;
+// let num2= 13;
+// let sum= num1+num2;
+// console.log(sum);
+// let product = num1 * num2 ;
+// console.log(product);
+
 
 
 // string operator (concatenation)
@@ -59,13 +60,68 @@ console.log(product);
 
 
 
-let arraysNum = [ 1, 2 , 3, 4, 5, 6  ];
-console.log(arraysNum);
+// let arraysNum = [ 1, 2 , 3, 4, 5, 6  ];
+// console.log(arraysNum);
 // accessed through index  starting from 0 to n-1   where n is the length of the array  
 
 
 /*let name= "abdisa";
 let  lname=" waritu ";*/
+
+// data types and datastructure 
+// data structure is a particular way of organizing  the data in a computer tha is can be used effectively 
+// console.log(typeof "Hello");
+// console.log(typeof 100);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof 123n);
+// console.log(typeof Symbol("id"));
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof function () {});
+// console.log(typeof null);
+// let firstName= " abdisa "
+// console.log (firstName);
+
+
+// arithemetic operation 
+let first_name = "abdisa "
+let last_name= " waritu"
+let full_name= first_name+ " "+ last_name;  // this is concatination + to merge the string in javascript 
+console.log(full_name);
+// these are convention  that 
+
+let  num1= 10;
+let num2="2";
+let sum = num2 + num1;
+console.log(sum);
+
+
+// equivalent comparision   inthe js 
+
+//==   equl to compares only the vales the assingm variable has  on vales works if the others have also the string with  compuares it doenst care about the data type it ignroe the tyeps of the  varibale also called the loose equality 
+// ===   values aas well the data types of the variable compare these two thigns only equl if these two condtion are met  that resulted in boolean  value eithier true or false  are also called the strict equality and its the recommended way  to use the triple equally since the js is the weak type  programming languages 
+// !=     &&    !==    the same is true for these comparstion  compauter operator 
+// javascritp compare the character by character by both of the operands are   strictly string they use the acsi code to compuare the given values and to reture the boolean answer if both the operands are string it s use the ACSI CODE 
+// let  first_name=  'abdisa';
+
+// type coercion 
+
+//  array declaration ;
+let fruits;
+fruits= ['banana', 'orange'];
+
+console.log(fruits.length);
+
+
+
+
+
+
+
+
+
+
 
 
 
