a3b563d8128feb3d6fa9c5297ca1bed8a6cf0191
Abdisa Waritu Ebiso <abdisawaritu4@gmail.com>
2026-08-10 09:57:52 -0700

    Updated Week- 11 Js Practice and Exercise

---
+++ Phase-2/Week-11/Javascript-Algorithm-Practice/JS/script.js
@@ -0,0 +1,8 @@
+function adder(number1, number2) {
+  let sum = number1 + number2;
+  let result = number1 * number2;
+}
+
+let myfirstName = "Abdi";
+let myfirstName = "Ali";
+let myfirstName = "Ahmed";
