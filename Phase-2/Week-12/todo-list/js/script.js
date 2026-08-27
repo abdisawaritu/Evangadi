11bbaed03b552c6070af6a4aba833301fe94c31a
Abdisa Waritu Ebiso <abdisawaritu4@gmail.com>
2026-08-27 07:01:06 -0700

    Add inital task data

---
+++ Phase-2/Week-12/todo-list/js/script.js
@@ -0,0 +1,32 @@
+let tasks = [
+  {
+    id: 1,
+    title: "Study JavaScript",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Complete frontend assignment",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Practice DOM manipulation",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Review JavaScript events",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Build a small frontend project",
+    completed: true,
+  },
+  {
+    id: 6,
+    title: "Push project to GitHub",
+    completed: false,
+  },
+];
