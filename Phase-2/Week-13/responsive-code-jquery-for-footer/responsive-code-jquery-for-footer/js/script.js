4906980399aa190f330b5c6d7ac0c566623f116f
Abdisa Waritu Ebiso <abdisawaritu4@gmail.com>
2026-09-01 23:28:56 -0700

    Updated js asychrounous and Jquery practice and Exercise

--- Phase-2/Week-13/responsive-code-jquery-for-footer/responsive-code-jquery-for-footer/js/script.js
+++ Phase-2/Week-13/responsive-code-jquery-for-footer/responsive-code-jquery-for-footer/js/script.js
@@ -1,21 +1,21 @@
-$(document).ready(function () {
-  /*
-    ==========================================
-    APPLE FOOTER MOBILE TOGGLE
-    ==========================================
-  */
+// $(document).ready(function () {
+//   /*
+//     ==========================================
+//     APPLE FOOTER MOBILE TOGGLE
+//     ==========================================
+//   */
 
-  $(".footer-links-wrapper h3").click(function () {
-    /*
-      Find the <ul> immediately after
-      the clicked <h3>.
-    */
-    $(this).next("ul").slideToggle(300);
+//   $(".footer-links-wrapper h3").click(function () {
+//     /*
+//       Find the <ul> immediately after
+//       the clicked <h3>.
+//     */
+//     $(this).next("ul").slideToggle(300);
 
-    /*
-      Add/remove the "expanded" class
-      from the clicked <h3>.
-    */
-    $(this).toggleClass("expanded");
-  });
-});
+//     /*
+//       Add/remove the "expanded" class
+//       from the clicked <h3>.
+//     */
+//     $(this).toggleClass("expanded");
+//   });
+// });
