7bec61061c51e06ecd46935e9450752f838afa05
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-11 11:49:12 -0700

    node modularity commonjs and ES6 standard practice and  Exercise

--- Phase-3/Week-15/node-module-and-web-server/node-module/abebe.js
+++ Phase-3/Week-15/node-module-and-web-server/node-module/abebe.js
@@ -1,1 +1,25 @@
-console.log(module);
+function sum(a, b) {
+  let c;
+  c = a + b;
+  console.log(c);
+}
+
+function x() {
+  console.log("this is the function from abebe.js");
+}
+let number = 5;
+
+// module.exports.abebe = number;
+// // exporting the function and variable here to use it inside other modules  for reusability. inside other modules
+// module.exports.sum = sum;
+
+// console.log(module);
+// those are availabe for the others to use thogh importing
+
+// for usage inside other modules to use this using require() function it can use inside the exports object.
+
+// ES6   standard that react uses importing and exporting  sytanx is different from the common js
+
+export default number;
+export { sum };
+export { x };
---
+++ Phase-3/Week-15/node-module-and-web-server/node-module/main.js
@@ -0,0 +1,10 @@
+const sum = require("./abebe"); // importing what others
+const sums = require("./kebede");
+sums.AAsomefunction();
+sum.AAsomefunction();
+// used to solve the global name  collision
+// console.log(sum);
+sum.sum(3, 5);
+
+// import number from "./abebe";
+// import { sum, x } from "./abebe";
