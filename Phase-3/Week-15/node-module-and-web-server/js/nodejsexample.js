6c5b967855083b3b72d9cb622644bb3a15b5c878
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-11 08:00:21 -0700

    node module practice and exercise

--- Phase-3/Week-15/node-module-and-web-server/js/nodejsexample.js
+++ Phase-3/Week-15/node-module-and-web-server/js/nodejsexample.js
@@ -1,2 +1,17 @@
 console.log("Hello js from node");
-console.log(window)
+// console.log(window);
+console.log(5 + 8);
+
+// node filename
+// node --watch filename
+// nodemon filename  using the npm of nodemon
+// var a  = 3
+// console.log(window.a);
+
+// console.log(global)
+// console.log(global.console)
+// let abebe = 1;
+
+// const _abebe = abebe;
+// export { _abebe as abebe };
+// console.log(module);
