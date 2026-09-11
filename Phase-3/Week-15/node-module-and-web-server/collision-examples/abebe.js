c115f0f54bd879f3f0e202820f0387196173a4e7
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-11 09:27:18 -0700

    Updated introduction to node modules practice and Exercise

---
+++ Phase-3/Week-15/node-module-and-web-server/collision-examples/abebe.js
@@ -0,0 +1,7 @@
+let  AAvar1 = 1;
+
+function AAsomeFunction (){
+    let AAsecond = 3 ;
+    console.log(AAvar1);
+}
+// console.log("olla")
---
+++ Phase-3/Week-15/node-module-and-web-server/collision-examples/kebede.js
@@ -0,0 +1,6 @@
+let AAvar3 =  3; 
+function  AAsomeFunction (){
+    let   a = 4;
+    console.log(AAvar3);
+}
+// console.log("abdisa")
