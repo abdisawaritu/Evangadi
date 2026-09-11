aac96a8a0933197e8561a9ff7e0e71287a30a682
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-11 08:36:43 -0700

    Update the  node modules practice and exercise

---
+++ Phase-3/Week-15/node-module-and-web-server/test.js
@@ -0,0 +1,7 @@
+console.log("Hello world ");
+
+// we are going to use three method to run the  js file or the modules inside the node enviroments
+//  node filename
+// node --watch filename
+// nodemon filename  by installing the  the nodemon package using  npm package
+console.log(4 + 5);
