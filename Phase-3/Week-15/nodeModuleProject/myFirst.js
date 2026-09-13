9a2e4b960343359fe70c2c2ea9673bd96b62a55e
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-13 09:42:18 -0700

    Updated node module exercise and practice

---
+++ Phase-3/Week-15/nodeModuleProject/myFirst.js
@@ -0,0 +1,20 @@
+// Question On Node Modules
+// 1.
+
+console.log("My First Module ");
+const myMultipler = (number) => {
+  return number * 2;
+};
+const myMulti = myMultipler(4);
+console.log(myMulti);
+
+module.exports = myMultipler;
+// module.exports = { myMultipler };
+
+// module.exports = { add, substract, multiple}  //  Exposing the  multiple functions
+// when we import the multiple function
+//  const add = {add , substract, multiply} = require ("./calculator ") here we use the destructuring of the object
+// const {add}  = require("./calcultor")
+// one function  module.exports = add;
+// multiple function  module.exports = {a , b,c }  is better to use  destructuring even if we have one function for the exposing it better to use
+console.log(module);
