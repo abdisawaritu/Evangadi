8a233c81a2cc4770a35c4f6dfd910f880c8ea1d8
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-14 05:21:25 -0700

    Updated  NOde module part one Exercise and Practice

---
+++ Phase-3/Week-15/nodeModuleProject/myCollector.js
@@ -0,0 +1,52 @@
+const myFirstMultipler = require("./myFirst");
+const mySecondMultiper = require("./mySecond");
+
+console.log(myFirstMultipler(5));
+console.log(mySecondMultiper(5));
+// console.log(myFirstMultipler)
+// console.log(mySecondMultiper)
+
+const firstResult = myFirstMultipler(14);
+const secondResult = mySecondMultiper(14);
+// console.log(firstResult, secondResult);
+
+const fs = require("fs");
+fs.writeFileSync(
+  "results.txt",
+  `The value of 14  when passed through the myMultipler function is ( ${firstResult})\n`,
+);
+
+fs.appendFileSync(
+  "results.txt",
+  `The value of 14  when passed through the myMultipler function is ( ${secondResult})`,
+);
+
+// Question number 4
+// const firstResult = myFirstMultipler(14);
+// const secondResult = mySecondMultiper(14);
+
+// const fs = require("fs");
+
+// fs.writeFile(
+//   "results.txt",
+//   `The value of 14 when passed through the myMultiplier function is ( ${firstResult}).\n`,
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+
+//     fs.appendFile(
+//       "results.txt",
+//       `The value of 14 when passed through the myMultiplier function is ( ${secondResult}).`,
+//       (err) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+
+//         console.log("Both results were written successfully.");
+//       },
+//     );
+//   },
+// );
---
+++ Phase-3/Week-15/nodeModuleProject/mySecond.js
@@ -0,0 +1,10 @@
+function myMultipler(number) {
+  return number * 3;
+}
+
+// console.log(myMultipler(4));
+
+const result = myMultipler(4);
+// console.log(result);
+
+module.exports =  myMultipler ;
