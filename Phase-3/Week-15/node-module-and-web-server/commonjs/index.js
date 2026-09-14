2d7921d74c702e27d66db5a41dca2e1da8c2e089
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-14 05:36:18 -0700

    Updated Node module Practice and Exercise

---
+++ Phase-3/Week-15/node-module-and-web-server/commonjs/index.js
@@ -0,0 +1,4 @@
+const { add , sub } = require("./math");  // object destructuint to use indviudal proptery
+
+console.log(add(1, 2));
+console.log(sub(3, 4))
---
+++ Phase-3/Week-15/node-module-and-web-server/commonjs/math.js
@@ -0,0 +1,14 @@
+let add = (x, y) => x + y;
+let sub = ( x, y ) => x - y;
+// module.exports.add = add;
+module.exports = {add , sub};
+// module.exports = add;
+
+
+
+// console.log(module);
+
+// let ex = {};
+
+// user.name = "abebe";
+// console.log(user); // object manipulation
