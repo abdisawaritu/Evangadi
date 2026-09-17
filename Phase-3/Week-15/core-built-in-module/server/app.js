3738bc055ead24b7e7b8ff3795dd46bdbfab9950
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-16 21:10:33 -0700

    Updated building webserver through http and express module  EXercise and Practice

--- Phase-3/Week-15/core-built-in-module/server/app.js
+++ Phase-3/Week-15/core-built-in-module/server/app.js
@@ -5,9 +5,19 @@ const http = require("http");
 // this method  turns a computer into an HTTP server
 
 const server = http.createServer((req, res) => {
-  res.end("Hello there ");
+  //   console.log(req.headers);
+  //   console.log(req.url);
+  //   console.log(req.method);
+
+  // what about on the response object
+  res.write("<h1> Hello world  </h1>");
+  res.write("<h1> Hello world  </h1>");
+  res.write("<h1> Hello world  </h1>");
+//   res.end();
+
+  res.end("<h1> May Batch  </h1>");
+
+  //   res.end("Hello there ");
 });
 
-server.listen(3000, function () {
-  console.log("It is Listening");
-});
+server.listen(3000);
