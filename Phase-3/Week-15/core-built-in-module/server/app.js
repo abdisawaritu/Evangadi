661b571bacc38e289520559efe6e7892da5bd092
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-17 11:33:53 -0700

    Updated building the webserver through http  and express framework   pracitce  and Exercise

--- Phase-3/Week-15/core-built-in-module/server/app.js
+++ Phase-3/Week-15/core-built-in-module/server/app.js
@@ -37,13 +37,32 @@
 // How to serve the static page  using the webserver
 
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  
-
-
-
-
-
-
+  let filePath = req.url;
+  if (req.url === "/") {
+    filePath = "index.html"; // the default is index.html from home page by default server index html serving the static page using the http module , fs module , and path modules 
+  }
+  let requestedPath = path.join(__dirname, "html", filePath);
+  let notFoundPath = path.join(__dirname, "html", "Nof-found.html");
+  fs.readFile(requestedPath, (error, content) => {
+    if (error) {
+      fs.readFile(notFoundPath , "utf8"  ,  (err , data)=>{
+        if(err){
+          console.log(error)
+        } else{
+          res.writeHead(404, { "content-type": "text/html" });
+          res.end(data)
+        }
+      });
+    } else {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(content);
+    }
+  });
+  // console.log(requestedPath );
 });
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Listening on http://localhost:3000");
+});
