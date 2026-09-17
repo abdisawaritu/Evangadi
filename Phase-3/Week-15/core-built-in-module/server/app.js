58463582a70ef5429f20c4d0335ab24e021ee691
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-17 12:21:13 -0700

    Updated building the webserver using the http module and express frmaework  practice and exercise

--- Phase-3/Week-15/core-built-in-module/server/app.js
+++ Phase-3/Week-15/core-built-in-module/server/app.js
@@ -39,30 +39,43 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const server = http.createServer((req, res) => {
-  let filePath = req.url;
-  if (req.url === "/") {
-    filePath = "index.html"; // the default is index.html from home page by default server index html serving the static page using the http module , fs module , and path modules 
-  }
-  let requestedPath = path.join(__dirname, "html", filePath);
-  let notFoundPath = path.join(__dirname, "html", "Nof-found.html");
-  fs.readFile(requestedPath, (error, content) => {
-    if (error) {
-      fs.readFile(notFoundPath , "utf8"  ,  (err , data)=>{
-        if(err){
-          console.log(error)
-        } else{
-          res.writeHead(404, { "content-type": "text/html" });
-          res.end(data)
-        }
-      });
-    } else {
-      res.writeHead(200, { "content-type": "text/html" });
-      res.end(content);
-    }
-  });
-  // console.log(requestedPath );
-});
-server.listen(3000, () => {
-  console.log("Listening on http://localhost:3000");
-});
+const { zstdCompress } = require("zlib");
+// const server = http.createServer((req, res) => {
+//   let filePath = req.url;
+//   if (req.url === "/") {
+//     filePath = "index.html"; // the default is index.html from home page by default server index html serving the static page using the http module , fs module , and path modules 
+//   }
+//   let requestedPath = path.join(__dirname, "html", filePath);
+//   let notFoundPath = path.join(__dirname, "html", "Nof-found.html");
+//   fs.readFile(requestedPath, (error, content) => {
+//     if (error) {
+//       fs.readFile(notFoundPath , "utf8"  ,  (err , data)=>{
+//         if(err){
+//           console.log(error)
+//         } else{
+//           res.writeHead(404, { "content-type": "text/html" });
+//           res.end(data)
+//         }
+//       });
+//     } else {
+//       res.writeHead(200, { "content-type": "text/html" });
+//       res.end(content);
+//     }
+//   });
+//   // console.log(requestedPath );
+// });
+// server.listen(3000, () => {
+//   console.log("Listening on http://localhost:3000");
+// });
+
+// Serving puppy Page 
+const puppyServer = http.createServer((req, res)=>{
+  let filePath =req.url;
+  if (filePath === "/"){
+    filePath = "/index.html"
+  } 
+
+  let requestedFile =  path.join(__dirname , "puppyPage" , filePath)
+})
+
+
