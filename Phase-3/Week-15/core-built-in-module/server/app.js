f6d8173f3ab2fcd9b0237ed77aa3f9b8810ff17c
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-16 21:39:23 -0700

    Updated building the webserver through the http and express framework  practice and  Exercise

--- Phase-3/Week-15/core-built-in-module/server/app.js
+++ Phase-3/Week-15/core-built-in-module/server/app.js
@@ -6,18 +6,30 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   //   console.log(req.headers);
-  //   console.log(req.url);
+    console.log(req.url);
   //   console.log(req.method);
 
   // what about on the response object
-  res.write("<h1> Hello world  </h1>");
-  res.write("<h1> Hello world  </h1>");
-  res.write("<h1> Hello world  </h1>");
-//   res.end();
-
-  res.end("<h1> May Batch  </h1>");
+  // res.write("<h1> Hello world  </h1>");
+  // res.write("<h1> Hello world  </h1>");
+  // res.write("<h1> Hello world  </h1>");
+  // //   res.end();
+  // res.setHeader("content-type", "text/html");
+  // res.writeHead(200, { "content-type": "text/html" });
+  // res.end("");
 
   //   res.end("Hello there ");
+
+  if (req.url ==="/"){
+    res.write("<h1> Home Page  </h1>");
+    res.end()
+  } else  if (req.url === "/about"){
+    res.write("<h1> About Page </h1>");
+  } else{
+
+    res.write("<h1> Page Not Found</h1>");
+  }
+  res.end();
 });
 
 server.listen(3000);
