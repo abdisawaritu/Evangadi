11b32df5a0acc34ef64a2a85c4ce7a1a73567a35
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-17 10:30:17 -0700

    Updated  building the webserver through the http module and express practice and Exercise

--- Phase-3/Week-15/core-built-in-module/server/app.js
+++ Phase-3/Week-15/core-built-in-module/server/app.js
@@ -1,35 +1,39 @@
+// const http = require("http");
+// // console.log(http);
+
+// // http.createServer([optio] , requestHandler(listener))
+// // this method  turns a computer into an HTTP server
+
+// const server = http.createServer((req, res) => {
+//   //   console.log(req.headers);
+//   // console.log(req.url);
+//   //   console.log(req.method);
+
+//   // what about on the response object
+//   // res.write("<h1> Hello world  </h1>");
+//   // res.write("<h1> Hello world  </h1>");
+//   // res.write("<h1> Hello world  </h1>");
+//   // //   res.end();
+//   // res.setHeader("content-type", "text/html");
+//   // res.writeHead(200, { "content-type": "text/html" });
+//   // res.end("");
+
+//   //   res.end("Hello there ");
+
+//   if (req.url === "/") {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write("<h1> Home Page  </h1>");
+//     res.end();
+//   } else if (req.url === "/about") {
+//     res.write("<h1> About Page </h1>");
+//   } else {
+//     res.write("<h1> Page Not Found</h1>");
+//   }
+//   res.end();
+// });
+
+// server.listen(3000);
+
+// How to serve the static page  using the webserver
+
 const http = require("http");
-// console.log(http);
-
-// http.createServer([optio] , requestHandler(listener))
-// this method  turns a computer into an HTTP server
-
-const server = http.createServer((req, res) => {
-  //   console.log(req.headers);
-    console.log(req.url);
-  //   console.log(req.method);
-
-  // what about on the response object
-  // res.write("<h1> Hello world  </h1>");
-  // res.write("<h1> Hello world  </h1>");
-  // res.write("<h1> Hello world  </h1>");
-  // //   res.end();
-  // res.setHeader("content-type", "text/html");
-  // res.writeHead(200, { "content-type": "text/html" });
-  // res.end("");
-
-  //   res.end("Hello there ");
-
-  if (req.url ==="/"){
-    res.write("<h1> Home Page  </h1>");
-    res.end()
-  } else  if (req.url === "/about"){
-    res.write("<h1> About Page </h1>");
-  } else{
-
-    res.write("<h1> Page Not Found</h1>");
-  }
-  res.end();
-});
-
-server.listen(3000);
