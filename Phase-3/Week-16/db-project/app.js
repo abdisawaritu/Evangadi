079ee1dec2cf4adc254f56aac1454da6262549cf
Abdisa Waritu <abdisawaritu4@gmail.com>
2026-09-23 21:05:01 -0700

    Updated working with database using the NODe js practice and exercise

--- Phase-3/Week-16/db-project/app.js
+++ Phase-3/Week-16/db-project/app.js
@@ -30,3 +30,50 @@ app.listen(3001, (error) => {
 });
 
 // how to create the database using the sql languages
+
+// Create all the tables
+
+app.get("/create-table", (req, res) => {
+  // puttin the query on the varialbe
+  let customers = `CREATE TABLE  IF NOT EXISTS  customers (
+  customer_id  INT  auto_increment,
+  name  varchar (255)  not null , 
+  PRIMARY KEY   (customer_id) 
+  )`;
+
+  let address = `CREATE TABLE  IF NOT EXISTS  address (
+   address_id INT  auto_increment,
+    customer_id  int(11) not null , 
+    address  varchar (255)  not null,
+  PRIMARY KEY   (address_id) ,
+  FOREIGN KEY  (customer_id)  REFERENCES customers  (customer_id)
+  )`;
+
+  let company = `CREATE TABLE IF NOT EXISTS  company  (
+       company_id   int auto_increment , 
+       customer_id  int(11)    not null, 
+       company varchar(255)  not null, 
+       PRIMARY KEY (company_id) , 
+       FOREIGN   KEY  (customer_id)  REFERENCES   customers(customer_id)
+  )`;
+
+  // executing  the querys  wr wrote  above
+
+  connection.query(customers, (error, result, field) => {
+    if (error) {
+      console.log(`Error Found ${error}`);
+    }
+  });
+
+  connection.query(address, (error, result, field) => {
+    if (error) {
+      console.log(`Error Found ${error}`);
+    }
+  });
+  connection.query(company, (error, result, field) => {
+    if (error) {
+      console.log(`Error Found ${error}`);
+    }
+  });
+  res.send("Tables Created Successfully");
+});
